7b90c536a6a3bd8635c5dec21353ac8d90db425a
theborch <theborch@gmail.com>
2015-07-07 09:35:07 -0500

    version bump to 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 . CHANGELOG
 ===========
 
+0.1.2
+-----
+- Will B - Cleanup of the readme.
+
 0.1.1
 -----
 - Will B - Addition of load_balancer resource.
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'will.borchardt@rackspace.com'
 license 'Apache 2.0'
 description 'Libraries for managing Rackspace cloud load balancers.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 depends 'xml'
